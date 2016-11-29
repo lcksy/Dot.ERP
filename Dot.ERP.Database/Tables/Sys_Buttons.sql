71ac9a3b938abe5a0fc6d5f0662f5613d2f77dc6
lcksy <421047431@qq.com>
2016-11-29 10:56:34 +0800

    20161129

---
+++ Dot.ERP.Database/Tables/Sys_Buttons.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sys_Buttons]
+(
+    [SysNo] INT NOT NULL PRIMARY KEY IDENTITY,
+    ButtonName VARCHAR(20) NOT NULL,
+    [ButtonNo] VARCHAR(20) NOT NULL,
+    [ButtonIcon] VARCHAR(20) NOT NULL,
+    [MenuNo] VARCHAR(50) NULL,
+    [CreateBy] VARCHAR(50) NULL
+)
